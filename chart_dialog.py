80f4a57d26721991e641dfa6640db7661e290a9c
UBags <67504653+UBags@users.noreply.github.com>
2026-03-23 03:51:40 +0530

    Update chart_dialog.py

--- chart_dialog.py
+++ chart_dialog.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2025 Uddipan Bagchi. All rights reserved.
-# See LICENSE in the project root for license information.package com.costheta.cortexa.action
+# See LICENSE in the project root for license information.
 
 """
 chart_dialog.py – Non-modal chart pop-ups for SWP Planner tables.
