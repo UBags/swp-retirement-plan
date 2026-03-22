1f721d67187c5211c7bdd2f47665b7b5f3b40b85
UBags <67504653+UBags@users.noreply.github.com>
2026-03-23 03:51:50 +0530

    Update check_pycache.py

--- check_pycache.py
+++ check_pycache.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2025 Uddipan Bagchi. All rights reserved.
-# See LICENSE in the project root for license information.package com.costheta.cortexa.action
+# See LICENSE in the project root for license information.
 
 """
 Quick diagnostic: run from your app directory.
